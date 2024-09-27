a7e3e6d9d2fa454d08cb4cc85abacc75f8048cd4
Carlos Bezerra <carlosromiobezerra@gmail.com>
2024-09-26 23:13:19 -0300

    Usuário
    parte basica de criação de usuários no banco de dados

--- remediario/src/main/java/br/edu/atitus/remediario/controllers/UserController.java
+++ remediario/src/main/java/br/edu/atitus/remediario/controllers/UserController.java
@@ -3,12 +3,9 @@ package br.edu.atitus.remediario.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import br.edu.atitus.remediario.entities.UserEntity;
+import br.edu.atitus.remediario.entities.UserEntity;  // Ajuste se necessário
 import br.edu.atitus.remediario.services.UserService;
 
 @RestController
@@ -16,11 +13,26 @@ import br.edu.atitus.remediario.services.UserService;
 public class UserController {
 
     @Autowired
-    private UserService userService; // Injetar corretamente o serviço
+    private UserService userService;
 
     @PostMapping
-    public ResponseEntity<Object> createUser(@RequestBody UserEntity user) {
-        ResponseEntity<Object> savedUserResponse = userService.saveUser(user);
-        return savedUserResponse; // Retornar a resposta gerada pelo serviço
+    public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity user) {
+        UserEntity savedUser = userService.saveUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/create")
+    public ResponseEntity<UserEntity> createUserByParams(
+        @RequestParam String email,
+        @RequestParam String password,
+        @RequestParam String name) {
+        
+        UserEntity user = new UserEntity();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setName(name);
+        
+        UserEntity savedUser = userService.saveUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 }
--- remediario/src/main/java/br/edu/atitus/remediario/services/UserService.java
+++ remediario/src/main/java/br/edu/atitus/remediario/services/UserService.java
@@ -1,13 +1,9 @@
 package br.edu.atitus.remediario.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import br.edu.atitus.remediario.model.User;
+import br.edu.atitus.remediario.entities.UserEntity; // Altere para importar UserEntity
 import br.edu.atitus.remediario.repositories.UserRepository;
-import org.springframework.dao.DataAccessException;
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,19 +11,7 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public ResponseEntity<Object> saveUser(User user) {
-        try {
-            // Exemplo de validação: verificar se o email já existe
-            Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
-            if (existingUser.isPresent()) {
-                return ResponseEntity.status(HttpStatus.CONFLICT).body("E-mail já cadastrado.");
-            }
-
-            User savedUser = userRepository.save(user);
-            return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
-
-        } catch (DataAccessException ex) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao salvar usuário: " + ex.getMessage());
-        }
+    public UserEntity saveUser(UserEntity user) { // Altere para UserEntity
+        return userRepository.save(user);
     }
 }
